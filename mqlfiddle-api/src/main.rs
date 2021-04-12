1054ed161c96c4d6354f802e7bbbfa841c3ae5b2
Ryan Chipman <ryan@ryanchipman.com>
2021-04-12 11:40:45 -0400

    actix-web boilerplate

--- mqlfiddle-api/src/main.rs
+++ mqlfiddle-api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind("localhost:8080")?
+        .run()
+        .await
 }
