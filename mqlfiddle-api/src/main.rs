a3b6ccd1357a018064db83708e2daab878c335c4
Ryan Chipman <ryan@ryanchipman.com>
2021-04-12 14:13:12 -0400

    api: Add endpoint for testing basic mongodb connectivity

--- mqlfiddle-api/src/main.rs
+++ mqlfiddle-api/src/main.rs
@@ -1,15 +1,36 @@
 use actix_web::{middleware::Logger, post, web, App, HttpServer};
-use serde::Serialize;
+use mongodb::{options::ClientOptions, Client};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 struct ExecuteResponse {
     result: String,
 }
 
+#[derive(Deserialize)]
+struct ExecuteRequest {
+    mql: String,
+}
+
+async fn listdbs() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+    let dbs = client.list_database_names(None, None).await?;
+    Ok(dbs)
+}
+
+#[post("/mqltest")]
+async fn mqltest() -> String {
+    let dbs = listdbs().await.unwrap();
+    dbs.join(", ")
+}
+
 #[post("/execute")]
-async fn execute() -> web::Json<ExecuteResponse> {
+async fn execute(info: web::Json<ExecuteRequest>) -> web::Json<ExecuteResponse> {
     let res = ExecuteResponse {
-        result: "I am a placeholder for the actual result docs".into(),
+        result: format!(
+            "I am a placeholder for the actual result docs. Your mql input was: {}",
+            info.mql
+        ),
     };
     web::Json(res)
 }
@@ -19,8 +40,13 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    HttpServer::new(|| App::new().wrap(Logger::default()).service(execute))
-        .bind("localhost:4000")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(execute)
+            .service(mqltest)
+    })
+    .bind("localhost:4000")?
+    .run()
+    .await
 }
