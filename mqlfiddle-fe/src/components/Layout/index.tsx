b4b110bcc8d3c24c870689a5865050035c588598
Nathan <terakilobyte@gmail.com>
2021-04-14 13:26:11 -0700

    adds multiple clients and executionStats
    Adds multiple clients in a `MongoClients` struct that is used in the
    data layer. The constituent clients are created with URIs derived from
    environmental variables. The version information for which client to
    used is passed in and checked, and the proper client is selected.
    
    Additionally, `executionStats` are now returned as part of aggregation
    execute. The driver doesn't expose a mechanism for explain other than
    `run_command`, and according to https://github.com/mongodb/specifications/blob/master/source/crud/crud.rst#q-a this is the proper way.

--- mqlfiddle-fe/src/components/Layout/index.tsx
+++ mqlfiddle-fe/src/components/Layout/index.tsx
@@ -59,115 +59,118 @@ const defaultMQL = JSON.stringify(
 const defaultVersion = VERSIONS[VERSIONS.length - 1].value;
 
 const Layout = () => {
-	const [schema, setSchema] = useState<string | undefined>(defaultSchema);
-	const [mql, setMql] = useState<string | undefined>(defaultMQL);
-	const [output, setOutput] = useState<string | undefined>(undefined);
-	const [version, setVersion] = useState(defaultVersion);
-	const { code } = useParams<{ code?: string }>();
-	const { addToast } = useToasts();
+  const [schema, setSchema] = useState<string | undefined>(defaultSchema);
+  const [mql, setMql] = useState<string | undefined>(defaultMQL);
+  const [output, setOutput] = useState<string | undefined>(undefined);
+  const [version, setVersion] = useState(defaultVersion);
+  const { code } = useParams<{ code?: string }>();
+  const { addToast } = useToasts();
 
-	useEffect(() => {
-		if (code) {
-			loadFiddle(code)
-				.then(({ data }) => {
-					setSchema(data.schema);
-					setMql(data.query);
-				})
-				.catch((e) => console.error(e));
-		}
-	}, [code]);
+  useEffect(() => {
+    if (code) {
+      loadFiddle(code)
+        .then(({ data }) => {
+          setSchema(data.schema);
+          setMql(data.query);
+          setVersion(data.version);
+        })
+        .catch((e) => console.error(e));
+    }
+  }, [code]);
 
-	const onExecute = () => {
-		executeFiddle({
-			schema: JSON.parse(schema!),
-			query: JSON.parse(mql!),
-		})
-			.then((res) => {
-				setOutput(JSON.stringify(res.data.result));
-				addToast("success", "Fiddle Executed", "Trace through the output");
-			})
-			.catch((e) => console.error(e));
-	};
+  const onExecute = () => {
+    executeFiddle({
+      schema: JSON.parse(schema!),
+      query: JSON.parse(mql!),
+      version,
+    })
+      .then((res) => {
+        setOutput(JSON.stringify(res.data.result));
+        addToast("success", "Fiddle Executed", "Trace through the output");
+      })
+      .catch((e) => console.error(e));
+  };
 
-	const onSave = () => {
-		saveFiddle({
-			schema: schema!,
-			query: mql!,
-		})
-			.then((res) => {
-				const saveUrl = `${window.location.protocol}//${window.location.host}/${res.data.code}`;
-				navigator.clipboard.writeText(saveUrl);
-				addToast(
-					"success",
-					"Fiddle Saved",
-					"Unique URL has been copied to clipboard"
-				);
-			})
-			.catch((e) => console.error(e));
-	};
+  const onSave = () => {
+    saveFiddle({
+      schema: schema!,
+      query: mql!,
+      version,
+    })
+      .then((res) => {
+        const saveUrl = `${window.location.protocol}//${window.location.host}/${res.data.code}`;
+        navigator.clipboard.writeText(saveUrl);
+        addToast(
+          "success",
+          "Fiddle Saved",
+          "Unique URL has been copied to clipboard"
+        );
+      })
+      .catch((e) => console.error(e));
+  };
 
-	const onReset = () => {
-		setMql(undefined);
-		setSchema(undefined);
-		if (output !== undefined) setOutput(undefined);
-		addToast("info", "Fiddle Reset", "Be Free!!");
-	};
+  const onReset = () => {
+    setMql(undefined);
+    setSchema(undefined);
+    if (output !== undefined) setOutput(undefined);
+    addToast("info", "Fiddle Reset", "Be Free!!");
+  };
 
-	const onLoadTemplate = () => {
-		setMql(defaultMQL);
-		setSchema(defaultSchema);
-		if (output !== undefined) setOutput(undefined);
-		addToast("info", "Fiddle Template Loaded", "Have Fun!!");
-	};
+  const onLoadTemplate = () => {
+    setMql(defaultMQL);
+    setSchema(defaultSchema);
+    if (output !== undefined) setOutput(undefined);
+    addToast("info", "Fiddle Template Loaded", "Have Fun!!");
+  };
 
-	const onVersionChange = (newVersion: string) => {
-		const _newVersion = isEmpty(newVersion) ? defaultVersion : newVersion;
-		setVersion(_newVersion);
-		addToast(
-			"success",
-			"Fiddle Version",
-			`MongoDB v${_newVersion} is active now`
-		);
-	};
+  const onVersionChange = (newVersion: string) => {
+    const _newVersion = isEmpty(newVersion) ? defaultVersion : newVersion;
+    setVersion(_newVersion);
+    addToast(
+      "success",
+      "Fiddle Version",
+      `MongoDB v${_newVersion} is active now`
+    );
+  };
 
-	return (
-		<div className="relative h-full w-full dark:bg-black">
-			<Navbar
-				onExecute={onExecute}
-				onSave={onSave}
-				onReset={onReset}
-				onLoadTemplate={onLoadTemplate}
-				isBlank={isEmpty(mql) && isEmpty(schema)}
-				version={version}
-				onVersionChange={onVersionChange}
-			/>
-			<div className="w-full flex flex-col content-container">
-				<div
-					className={clsx("w-full flex flex-none", output ? "h-3/5" : "h-full")}
-				>
-					<div className="w-1/2 h-full py-1.5">
-						<Editor
-							data={schema}
-							setData={setSchema}
-							title="Data"
-							defaultLanguage="json"
-						/>
-					</div>
-					<div className="border-r border-gray-light dark:border-gray-dark h-full" />
-					<div className="w-1/2 h-full py-1.5">
-						<Editor
-							data={mql}
-							setData={setMql}
-							title="MQL"
-							defaultLanguage="json"
-						/>
-					</div>
-				</div>
-				<Output output={output} />
-			</div>
-			<ThemeToggle />
-		</div>
-	);
+  return (
+    <div className="relative h-full w-full dark:bg-black">
+      <Navbar
+        onExecute={onExecute}
+        onSave={onSave}
+        onReset={onReset}
+        onLoadTemplate={onLoadTemplate}
+        isBlank={isEmpty(mql) && isEmpty(schema)}
+        version={version}
+        onVersionChange={onVersionChange}
+      />
+      <div className="w-full flex flex-col content-container">
+        <div
+          className={clsx("w-full flex flex-none", output ? "h-3/5" : "h-full")}
+        >
+          <div className="w-1/2 h-full py-1.5">
+            <Editor
+              data={schema}
+              setData={setSchema}
+              title="Data"
+              defaultLanguage="json"
+            />
+          </div>
+          <div className="border-r border-gray-light dark:border-gray-dark h-full" />
+          <div className="w-1/2 h-full py-1.5">
+            <Editor
+              data={mql}
+              setData={setMql}
+              title="MQL"
+              defaultLanguage="json"
+            />
+          </div>
+        </div>
+        <Output output={output} />
+      </div>
+      <ThemeToggle />
+    </div>
+  );
 };
 
 export default Layout;
