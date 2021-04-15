52935d69e14efeecb0f037642d1e142714f60326
Ryan Chipman <ryan@ryanchipman.com>
2021-04-15 14:53:55 -0400

    tmp select

--- mqlfiddle-fe/src/components/LoadDialog/index.tsx
+++ mqlfiddle-fe/src/components/LoadDialog/index.tsx
@@ -1,41 +1,72 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
 import ConfirmationModal from "@leafygreen-ui/confirmation-modal";
 import TextInput from "@leafygreen-ui/text-input";
+import { Select, Option, OptionGroup, Size } from "@leafygreen-ui/select";
+import axios from "axios";
 
 interface LoadDialogProps {
-  open: boolean;
-  onLoad: () => void;
-  onCancel: () => void;
-  fiddleId: string;
-  setFiddleId: Dispatch<SetStateAction<string>>;
+	open: boolean;
+	onLoad: () => void;
+	onCancel: () => void;
+	fiddleId: string;
+	setFiddleId: Dispatch<SetStateAction<string>>;
 }
 
+const getMyFiddleCodes = () => (
+	axios({
+		method: 'get',
+		url: '/api/current_user/my_fiddles',
+	}).then(
+		res => res.data.fiddle_codes,
+	).catch(e => {
+		console.error(e)
+	})
+);
+
 const LoadDialog = ({
-  open,
-  onLoad,
-  onCancel,
-  fiddleId,
-  setFiddleId,
+	open,
+	onLoad,
+	onCancel,
+	fiddleId,
+	setFiddleId,
 }: LoadDialogProps) => {
-  return (
-    <ConfirmationModal
-      open={open}
-      onConfirm={onLoad}
-      onCancel={onCancel}
-      title="Load A Saved Fiddle"
-      buttonText="Load"
-      submitDisabled={fiddleId === ""}
-    >
-      <TextInput
-        className="mt-10"
-        aria-labelledby="load-input"
-        autoComplete="off"
-        onChange={(event) => setFiddleId(event.target.value)}
-        value={fiddleId}
-        placeholder="Code"
-      />
-    </ConfirmationModal>
-  );
+	const [myFiddleCodes, setMyFiddleCodes] = useState<Array<string>>([]);
+	const [selectedFiddle, setSelectedFiddle] = useState<string | undefined>();
+
+	useEffect(() => {
+		getMyFiddleCodes().then(setMyFiddleCodes)
+	}, []);
+
+	return (
+		<ConfirmationModal
+			open={open}
+			onConfirm={onLoad}
+			onCancel={onCancel}
+			title="Load A Saved Fiddle"
+			buttonText="Load"
+			submitDisabled={fiddleId === ""}
+		>
+			<Select
+				aria-labelledby="load-select"
+				size={Size.Default}
+				onChange={val => console.log(val)} >
+				<Option value="from-code">From Code</Option>
+				<OptionGroup label="My Fiddles">
+					{['one', 'two', 'three'].map(code => (
+						<Option value={code} key={code}>{code}</Option>
+					))}
+				</OptionGroup>
+			</Select>
+			<TextInput
+				className="mt-10"
+				aria-labelledby="load-input"
+				autoComplete="off"
+				onChange={(event) => setFiddleId(event.target.value)}
+				value={fiddleId}
+				placeholder="Code"
+			/>
+		</ConfirmationModal>
+	);
 };
 
 export default LoadDialog;
